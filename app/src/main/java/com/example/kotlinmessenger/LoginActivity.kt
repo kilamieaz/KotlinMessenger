e980af7e3afd5b8a1feac908acfd745f4a33c660
sultan imam muttaqin <im.sultaan05@gmail.com>
2019-05-24 14:09:11 +0700

    login-register-firebase

--- app/src/main/java/com/example/kotlinmessenger/LoginActivity.kt
+++ app/src/main/java/com/example/kotlinmessenger/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.kotlinmessenger
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity() {
@@ -14,6 +15,15 @@ class LoginActivity: AppCompatActivity() {
             val email = email_edittext_login.text.toString()
             val password = password_edittext_login.text.toString()
             Log.d("Login", "Attempt login with email/pw: $email/***")
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if (!it.isSuccessful) return@addOnCompleteListener
+                    // else if successful
+                    Log.d("Login", "Successfully Login user with uid: ${it.result!!.user.uid}")
+                }
+                .addOnFailureListener{
+                    Log.d("Login", "Failed to Login: ${it.message}")
+                }
         }
 
         back_to_register_textview.setOnClickListener {
